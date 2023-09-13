8dccf4030a0af63d9093bf90225572f93f57f367
Vigyan Madan <vigyanm@carbonblack.com>
2023-09-13 11:52:20 -0700

    count consecutively

---
+++ count_letters(tiktok)/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func countLetters(in string) {
+	var curChar rune
+	curCount := 0
+
+	for _, char := range in {
+		if char == curChar {
+			curCount++
+		} else {
+			if curCount > 0 {
+				fmt.Printf("%c : %d\n", curChar, curCount+1)
+			}
+			curChar = char
+			curCount = 0
+		}
+
+	}
+	if curCount > 0 {
+		fmt.Printf("%c : %d\n", curChar, curCount+1)
+	}
+}
+
+func main() {
+	in := "ttttiikkk ttooookkk"
+	countLetters(in)
+}
